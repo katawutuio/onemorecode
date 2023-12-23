e6ab2e4b8d485c3e6b41f656702582515114b0fd
katawut <katawutu.io@gmail.com>
2023-12-23 20:03:38 +0700

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `onemorecode.dev`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
